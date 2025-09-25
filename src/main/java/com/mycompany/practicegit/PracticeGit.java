6872efcbc44b070f3d8f65f7c75d93e768036c0a
bhardkri <bhardkri@sheridancollege.ca>
2025-09-25 08:31:06 -0400

    Version 1.

---
+++ src/main/java/com/mycompany/practicegit/PracticeGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicegit;
+
+/**
+ *
+ * @author krish
+ */
+public class PracticeGit {
+
+    public static void main(String[] args) {
+        System.out.println("Code Version One!");
+    }
+}
